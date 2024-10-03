bef37bece1a85e29ada7c8d7efdb627e01c4d13e
sheithenfar <92501511+sheithenfar@users.noreply.github.com>
2024-10-03 15:31:07 +0800

    Add files via upload

---
+++ file/Python機器學習應用開發_22_20231206.py
@@ -0,0 +1,13 @@
+# demo(test)
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("Iris")
+st.text("Iris")
+st.write("Iris")
+st.info("Iris")
+st.write("# Iris") #Markdown
+
+a = np.array([[1,2],[3,4],[5,6]])
+st.write(a)
